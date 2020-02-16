9b0ef16fd404b6bb6a9892f2fb8be41e0e045ca6
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-02-16 08:34:42 +0000

    Initial commit

---
+++ first.py
@@ -0,0 +1,7 @@
+name = input("name? ")
+age = int(input("age: "))
+value = 100 - age
+year = 2020 + value
+
+number = int(input("enter some nuber "))
+print(number * (f'{name} you will turn 100 years old in {value} years and that is in the year {year}\n'))
---
+++ second.py
@@ -0,0 +1,4 @@
+number = input("number")
+if number%2 ==0:
+  print("even") 
+print("odd")
