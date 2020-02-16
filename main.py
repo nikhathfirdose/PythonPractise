ac18fbbe35be95840559ef5aece77685cfd4bd3f
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-02-16 08:52:44 +0000

    3rd

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#second
-number = int(input("number"))
-if number%2==0:
-  print("even")
-  if number%4==0:
-    print("multiple of 4")
-else:
+#3
+k =[]
+l = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in l:
+  if i<5:
+    k.append(i)
+print(k)
 
-  print("odd")
+
