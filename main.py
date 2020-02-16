29a5cef91c515c7b390063831f52edecf5f69147
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-02-16 08:44:03 +0000

    2nd

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#first
-name = input("name? ")
-age = int(input("age: "))
-value = 100 - age
-year = 2020 + value
-
-number = int(input("enter some nuber "))
-print(number * (f'{name} you will turn 100 years old in {value} years and that is in the year {year}\n'))
-
 #second
-number = input("number")
-if number%2 ==0:
-  print("even") 
-print("odd")
+number = int(input("number"))
+if number%2==0:
+  print("even")
+  if number%4==0:
+    print("multiple of 4")
+else:
+
+  print("odd")
