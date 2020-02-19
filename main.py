c44706937b208dafe595916709cdc90e20d043aa
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-02-19 11:39:06 +0000

    Duplicates in a list Exercise

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-#Exercise!
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,1,0,1,0,0,0],
-  [0,1,1,1,0,0,0]
-]
-for image in picture:
-  for pixel in image:
-    if pixel == 0:
-      print(" ", end = " ")
-    else:
-      print("*",end = " ")
-  print(" ")
+#Exercise
+#find duplicates of the list
+s = ['a','b','c','b','n','n','m']
+for i in range(len(s)):
+  for j in range(i+1,len(s)):
+    if s[i]==s[j]:
+      print(s[i])
+#andres solution
+
+dup = []
+for value in s:
+  if s.count(value) >1:
+    if value not in dup:
+      dup.append(value)
+print(dup)
