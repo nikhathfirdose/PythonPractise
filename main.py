3ebc7fad472a2c306e1a2385b4f07ba15b8c4f6a
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-02-19 17:54:21 +0000

    Exercise Highest even

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-def checkDriver(age):
-  
-  if int(age) < 18:
-	  print("Sorry, you are too young to drive this car. Powering off")
-  elif int(age) > 18:
-	  print("Powering On. Enjoy the ride!");
-  elif int(age) == 18:
-	  print("Congratulations on your first year of driving. Enjoy the ride!")
+def highestEven(li):
+  even = []
+  for i in li:
+    if i%2 == 0:
+      even.append(i)
+  return max(even)
 
-checkDriver(92)
+print(highestEven([1,2,3,45,-7,5,6,7778,8]))
 
-#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
-# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+#Exercise find highest even number
 
-#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
-#checkDriverAge(92);
-#it returns "Powering On. Enjoy the ride!"
-#also make it so that the default age is set to 0 if no argument is given.
+
+
 
