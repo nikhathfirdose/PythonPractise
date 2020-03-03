757ea9c57ddb253ea52469ba8cb06d8dc2f7108e
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-03 15:20:28 +0000

    green

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-class Cat:
-    species = 'mammal'
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def walk(self):
+        return f'{self.name} is just walking around'
 
-# Instantiate the Cat object with 3 cats
-peanut = Cat("Peanut", 3)
-garfield = Cat("Garfield", 5)
-snickers = Cat("Snickers", 1)
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-# Find the oldest cat
-def get_oldest_cat(*args):
-    return max(args)
+#1 Add nother Cat
 
-    #here function is outside so we can acces directly
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = []
 
+#3 Instantiate the Pet class with all your cats use variable my_pets
 
-# Output
-print(f"The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old.")
+#4 Output all of the cats walking using the my_pets instance
+#committttts
