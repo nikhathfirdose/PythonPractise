8fd4fa221754f744860daa1ad794482262a3a54e
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-10 10:58:28 +0000

    Static and Dynamic attributes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-class PlayerCharacter():
-  def __init__(self, name):
-    self.name = name #attributes
-  def run(self): #method
-    print("Yayayy")
-p1 = PlayerCharacter("Nikhath")
-(p1.run()) #since its a method it need not print twice else like the next statement
-print(p1.run()) #it will return a "None"value
-print(p1.name)
+class Player:
+  #class Attribute
+  member = True 
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def run(self):
+    return "my name is" + self.name # return use "+" forrint use ","
+
+p1 = Player("cindy", 6)
+p2 = Player("raj", 56)
+print(p2.run())  #these both change == Dynamic
+print(p1.run()) 
+print(p1.member) #always true == Static
 
