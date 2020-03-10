f640b8cf682e06ef8286d79525e1d78eb7022fda
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-10 12:48:44 +0000

    Cat Exercise

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-class Player:
-  #class Attribute
-  member = True 
-  def __init__(self, name, age):
+#Exercise Cat 2nd Time
+
+class Cat:
+  def __init__(self,name,age):
     self.name = name
     self.age = age
-  def run(self):
-    return "my name is" + self.name # return use "+" forrint use ","
+  
+  def Oldest(*args):
+    return max(args)
 
-p1 = Player("cindy", 6)
-p2 = Player("raj", 56)
-print(p2.run())  #these both change == Dynamic
-print(p1.run()) 
-print(p1.member) #always true == Static
+c1 = Cat("pp", 8)
+c2 = Cat('oo',8)
+c3 = Cat("poil", 9)
 
+print(f'the oldest is {Cat.Oldest(c1.age,c2.age,c3.age)} years old')
