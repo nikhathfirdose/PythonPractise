9619a8acc362d01457c83650547ecc5a964fd37c
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-10 10:33:55 +0000

    Class, Methods and Attributes

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-class Pets():
-    animals = []
-    def __init__(self, animals):
-        self.animals = animals
+class PlayerCharacter():
+  def __init__(self, name):
+    self.name = name #attributes
+  def run(self): #method
+    print("Yayayy")
+p1 = PlayerCharacter("Nikhath")
+(p1.run()) #since its a method it need not print twice else like the next statement
+print(p1.run()) #it will return a "None"value
+print(p1.name)
 
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
-
-class Cat():
-    is_lazy = True
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def walk(self):
-        return f'{self.name} is just walking around'
-
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-#1 Add nother Cat
-
-#2 Create a list of all of the pets (create 3 cat instances from the above)
-my_cats = []
-
-#3 Instantiate the Pet class with all your cats use variable my_pets
-
-#4 Output all of the cats walking using the my_pets instance
-#c
