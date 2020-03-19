b31e84a3eb64c5d86d5244021799db2f45e1ca50
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-19 15:56:43 +0000

    why map is a pure function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #pure function eample- compile
-
+li = [1,2,3]
 def mul(i):
   return i*2
 
-print(list(map(mul,[1,2,3])))
+print(list(map(mul,li)))
+print(li)
+#so map did the mapping but also kept the original list same, so it is a pure function coz has no "side-effects"
