d5bf3cd74320d4386fb1b3e9d734dc6d04b33db3
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-19 15:13:48 +0000

    pure functions, with side effects

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-#exercise pets again 
-class Pets():
-    animals = [] #class atrribute == static
-    def __init__(self, animals):
-        self.animals = animals
-
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
-
-class Cat(): 
-    is_lazy = True
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def walk(self):
-        return f'{self.name} is just walking around'
-
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Pete(Cat):
-    def sing(self, sounds):
-      return f'{sounds}'
-
-#1 Add nother Cat
-
-#2 Create a list of all of the pets (create 3 cat instances from the above)
-my_cats = []
-
-#3 Instantiate the Pet class with all your cats use variable my_pets
-
-#4 Output all of the cats walking using the my_pets instance
+#pure function eample
+nli =[] # since ths is outside the scope of the funcion, this will have side-effects
+def mulby2(li):
+  
+  for item in li:
+    nli.append(item*2)
+  return nli
+print(mulby2([1,2,3]))
