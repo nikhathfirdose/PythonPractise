e71762d3e4addb54b46ca586d15bac896b081756
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-19 15:55:10 +0000

    map

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#pure function eample
+#pure function eample- compile
 
-def mulby2(li):
-  nli =[] 
-  for item in li:
-    nli.append(item*2)
-  return nli
-nli = "" #this affects if nli is in the outside scope, but has no affect if nli is paced inside
-print(mulby2([1,2,3]))
+def mul(i):
+  return i*2
+
+print(list(map(mul,[1,2,3])))
