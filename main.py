d78a0322271af5123c483a930b76a3096125a0ce
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-03-20 08:26:13 +0000

    comprehensions

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-#squares
-my_ist = [5,4,3]
-print(list(map(lambda i:i **2, my_ist)))
+#list comprehensions
+li = [i for i in range(0,100) if i%2==0]
+print(li)
+
+#set - same as lists
+si = { i for i in [1,2,3,3,6,33,4]}
+print(si)
+
+#dict comprehensions
+sample = {
+  "a":5,
+  "b": 6
+}
+new_d = {k:v**2 for k,v in sample.items()}
+print(new_d)
 
 
-#list sort
-a= [(0,2), (4,1), (9,9), (10, -1)]
-a.sort(key=lambda i: i[1])
-print(a) 
+#dict copre hension by just using key
+dics = { k: k*2 for k in [1,2,3]}
+print(dics)
